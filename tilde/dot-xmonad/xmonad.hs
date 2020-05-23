b3807d1ba47aca9ee5fd66fe42d72af26e69dca4
JEphron <jephron63@gmail.com>
2020-05-23 13:41:34 -0700

    Bunch of random stuff, finally figured out how to have a floating window that doesn't steal focus

--- tilde/dot-xmonad/xmonad.hs
+++ tilde/dot-xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import Data.Map ( fromList )
+import Data.Monoid ( Endo )
 import System.Exit ( exitSuccess )
 
 import XMonad
@@ -7,8 +8,11 @@ import XMonad.Layout.NoBorders ( smartBorders )
 import XMonad.Hooks.ManageDocks ( avoidStruts, manageDocks )
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog ( xmobar )
-import XMonad.Hooks.ManageHelpers ( composeOne, (-?>), isDialog, doCenterFloat, transience, doSideFloat, Side( SW ) )
+import XMonad.Hooks.RefocusLast 
 
+import XMonad.Hooks.ManageHelpers ( composeOne, (-?>), isDialog, doCenterFloat, transience, doSideFloat, Side( SW, SE ) )
+
+import XMonad.Actions.Launcher
 import XMonad.Actions.GroupNavigation ( Direction( History ), historyHook, nextMatch )
 import XMonad.Layout.MultiToggle ( mkToggle, single, Toggle(Toggle) )
 import XMonad.Layout.MultiToggle.Instances ( StdTransformers( FULL ) )
@@ -25,44 +29,55 @@ import XMonad.Prompt.FuzzyMatch as Fuzzy
 superKey = mod1Mask
 
 myFont = "'xos4 Terminus:size=14'"
-stConfig = " -f " ++ myFont
+stConfig = "" --" -f " ++ myFont
 
 termCommand = "st" ++ stConfig
+
+launcherConfig = LauncherConfig
+    { 
+      pathToHoogle = "/home/jephron/.local/bin/hoogle"
+    , browser = "firefox"
+    }
 
 myConfig = def
             {
                 terminal            = termCommand
               , focusFollowsMouse   = False
-              , modMask             = mod4Mask
+              , modMask             = mod1Mask
               , startupHook = setWMName "LG3D"
               , focusedBorderColor  = "#335566"
               , normalBorderColor   = "#181000"
-              , logHook             = historyHook
+              , logHook             = refocusLastLogHook <+> historyHook
               , layoutHook          = avoidStruts $ smartBorders $ myLayout
               , manageHook          = myManageHook <+> manageHook defaultConfig <+> manageDocks
             } `additionalKeysP` [
-                ("M-S-t", spawn "st")                                           -- new terminal
-              , ("M-S-<Return>",trySpawnShellAtWindowCwd)                          -- new terminal at cwd
-              , ("M-C-q", spawn "slock")                                        -- lock the screen
-              , ("M-p",   shellPrompt myXPConfig)                               -- app launcher
-              , ("M-S-f", sendMessage $ Toggle FULL)                            -- toggle fullscreen
-              , ("M-m",   nextMatch History (return True))                      -- MRU
-              , ("M-S-s", withFocused $ windows . W.sink)                       -- Push window back into tiling
-              , ("M-S-q", confirmPrompt myXPConfig "exit" (io exitSuccess))     -- confirm quit x
+                ("M-S-t", spawn "st")                                                     -- new terminal
+              , ("M-S-<Return>",trySpawnShellAtWindowCwd)                                 -- new terminal at cwd
+              , ("M-C-q", spawn "slock")                                                  -- lock the screen
+              , ("M-<Space>",   shellPrompt myXPConfig)                                         -- app launcher
+              , ("M-C-f", sendMessage $ Toggle FULL)                                      -- toggle fullscreen
+              -- , ("M-m",   nextMatch History (return True))                                -- MRU
+              , ("M-S-s", withFocused $ windows . W.sink)                                 -- Push window back into tiling
+              , ("M-S-q", confirmPrompt myXPConfig "exit" (io exitSuccess))               -- confirm quit x
               -- applications
-              , ("M-x M-f", spawn "firefox")                                    -- spawn firefox
-              , ("M-x M-r", spawn (termCommand ++ " ranger"))                           -- spawn ranger
-              , ("M-x M-e", spawn "emacsclient -c")                             -- spawn emacs
-              , ("M-S-p",   spawn "ffcast -s png ~/Screenshots/\"$(date +%F\\ %T)\".png")   -- take screenshot
-              , ("<XF86AudioLowerVolume>",   spawn "amixer sset Master 10-")
-              , ("<XF86AudioRaiseVolume>",   spawn "amixer sset Master 10+")
-              , ("<XF86AudioMute>", spawn "amixer -q set Master toggle")
+              , ("M-x M-f", spawn "firefox")                                              -- Firefox
+              , ("M-x M-r", spawn (termCommand ++ " ranger"))                             -- Ranger file browser
+              , ("M-x M-i", spawn "idea")                                                 -- IntelliJ IDEA
+              , ("M-x M-e", spawn "emacs")                                       -- Emacs
+              , ("M-x M-l", launcherPrompt myXPConfig $ defaultLauncherModes launcherConfig)
+              , ("M-S-p",   spawn "ffcast -s png ~/Screenshots/\"$(date +%F\\ %T)\".png") -- take screenshot
+              , ("<XF86AudioLowerVolume>" , spawn "pactl set-sink-mute 0 false; pactl set-sink-volume 0 -5%")
+              , ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-mute 0 false; pactl set-sink-volume 0 +5%")
+              , ("<XF86AudioMute>", spawn "pactl set-sink-mute 0 toggle")
               --, ("M-S-<Tab>", cycleRecentWS' [xK_Super_L, xK_Shift_L] xK_Tab xK_grave) ppp PPP
             ] `removeKeysP` [
                 "M-<Return>",
                 "M-S-a",
                 "M-S-o",
-                "M-S-p"
+                "M-S-p",
+                "M-S-r",
+                "M-r",
+                "M-p"
             ]
 
 -- modified variant of cycleRecentWS from XMonad.Actions.CycleRecentWS (17)
@@ -148,12 +163,17 @@ myLayout = mkToggle (single FULL) (tiled ||| Mirror tiled ||| emptyBSP)
         delta = 3/100
 
 
+doX :: X () -> Query (Endo WindowSet)
+doX x = (liftX x) >> (doF id)
+
 --------------------------------------------------------------------------------
 myManageHook = composeAll
   [ className =? "mpv"    --> doFloat
   -- , className =? "processing-core-PApplet"   --> doSideFloat SW
   -- , className =? "processing-core-PApplet"   --> doF (fmap W.sink . W.peek )
   , isDialog              --> doCenterFloat
+  , title =? "FOOBAR"     --> doSideFloat SE <> (doF W.focusDown)-- >> doX (toggleFocus)  --  >> >> --doF (W.focusMaster)
+  
   -- , transience                                   -- Move transient windows to their parent:
   ]
 
